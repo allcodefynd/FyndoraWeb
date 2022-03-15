e3ac0b1e5a05d50125ed6b30837840d66e926722
Fyndora <info@fyndoratech.com>
2022-03-15 22:13:32 +0530

    something change

--- fyndoraWeb/fyndoratech/src/view/home/BannerDownSecond/bannersecond.js
+++ fyndoraWeb/fyndoratech/src/view/home/BannerDownSecond/bannersecond.js
@@ -14,7 +14,7 @@ function BannerSecond() {
                         <img src={process.env.PUBLIC_URL + "/Assets/computerDesign.svg"} alt="logo" />
                     </Col>
                     <Col>
-                        <h2 style={{ textAlign: "center" }}>Fyndora Technology</h2>
+                        <h2 style={{ textAlign: "center" }}>InDIAn Technology</h2>
                         <span className="content_bar"></span>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </Col>
